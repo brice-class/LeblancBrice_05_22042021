9d387cebf36dfbab08dfa49ffc659b8b16e41acd
Brice Leblanc <d2_brice@hotmail.fr>
2021-08-05 00:26:50 +0200

    page index plus dynamique

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 const container = document.getElementById("global-list")
 
- const url = "http://localhost:3000/api/teddies";
+ const url = "http://localhost:3000/api/";
+ let produit = "teddies";
+ loadtData(); 
+
+ function loadtData(){
  let requete = new XMLHttpRequest(); // créer un objet 
- requete.open("get", url); // premier parametre :  get / post, deuxieme parametre : url
+ requete.open("get", url + produit); // premier parametre :  get / post, deuxieme parametre : url
  requete.responseType = "json"; //Nous attendons du Json
  requete.send(); //Nous envoyons notre requete 
-
-
  requete.onload = function(){ //lorsque la requete est prete 
      if (requete.readyState === XMLHttpRequest.DONE){
          if (requete.status === 200) {
              let reponse = requete.response; //on stock la réponse.  
-             let teddies = reponse;
-             console.log(teddies) ;
-
-             teddies.forEach(teddy => {
+             let articles = reponse;
+             console.log(articles) ;
+             container.innerHTML="";
+             articles.forEach(article => {
                 const card = `<div class="col-12 col-lg-4 ">
-                                <a href="/produit.html?id=${teddy._id}">
+                                <a href="/produit.html?id=${article._id}">
                                     <div class="card">
-                                        <img src="${teddy.imageUrl}" alt="" class="card-img-top">
+                                        <img src="${article.imageUrl}" alt="" class="card-img-top">
                                         <div class="card-body">
-                                            <h5 class="card-title">${teddy.name}</h5>
-                                            <p class="card-text">${teddy.description}</p>
-                                            <p class="card-text"> ${teddy.price} €</p>
+                                            <h5 class="card-title">${article.name}</h5>
+                                            <p class="card-text">${article.description}</p>
+                                            <p class="card-text"> ${article.price} €</p>
                                         </div>
                                     </div>
                                 </a>
@@ -34,6 +36,14 @@ const container = document.getElementById("global-list")
              alert("Un probleme est intervenu, merci de revenir plus tard");
          }
     }
+}
+}
+
+function modifierArticle(){
+    var select_article = document.getElementById("article");
+    console.log(select_article.value);
+    produit = select_article.value;
+    loadtData();
 }
 
 
